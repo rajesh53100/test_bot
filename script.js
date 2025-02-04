5a51ae67508ccbc3c1b27c248bcbb53f40193fcc
rajesh53100 <167516568+rajesh53100@users.noreply.github.com>
2025-02-04 17:11:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@
         } else {
             window.location = `https://cloud.yellow.ai/pwa/v2/live/x1714103142268`
         }
-    }else if(url.includes('pwa/live/x1545629713127') || url.includes('pwa/live/x1545990126801') || url.includes('pwa/live/x1545990145814'){
+    }else if(url.includes('pwa/live/x1545629713127') || url.includes('pwa/live/x1545990126801') || url.includes('pwa/live/x1545990145814') ){
         window.location = `https://cloud.yellow.ai/pwa/v2/live/x1714103142268`
     }
 
